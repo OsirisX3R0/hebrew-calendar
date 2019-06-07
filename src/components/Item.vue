7796cd2cdb0844bb829015f208ff24699b5c1bc4
Richard Harris <richard@idestini.com>
2019-06-07 17:08:26 -0500

    Added fetch, calendar data attained

---
+++ src/components/Item.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-card>
+      <ion-card-header>
+        <ion-card-subtitle>Item Subtitle</ion-card-subtitle>
+        <ion-card-title>Item Title</ion-card-title>
+      </ion-card-header>
+      <ion-card-content>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iusto a possimus mollitia quibusdam, itaque illum modi reprehenderit cumque sed ea aliquam beatae enim debitis. Dolor eius perspiciatis aut possimus?
+      </ion-card-content>
+    </ion-card>
+</template>
+
+<script>
+export default {
+    name: "Item",
+    data() {
+        props: ["item"]
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,25 @@
       </ion-toolbar>
     </ion-header>
 
-    <ion-card>
-      <ion-card-header>
-
-      </ion-card-header>
-      <ion-card-content>
-
-      </ion-card-content>
-    </ion-card>
+    <Item />
   </div>
 </template>
 
 <script>
+import Item from '../components/Item';
 export default {
   name: 'home',
   components: {
-    
-  }
+    Item
+  },
+  data() {
+    return {
+      calendar: null
+    }
+  },
+  async created() {
+    const res = await fetch(`https://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=off&mod=on&nx=on&year=now&month=${new Date().getMonth() + 1}&ss=on&mf=on&c=on&geo=zip&zip=72117&m=50&s=on`);
+    this.calendar = await res.json();
+  } 
 }
 </script>
